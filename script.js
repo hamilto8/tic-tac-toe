8384d8fe9c7381000d9effc19c581003479ded55
Michael <michael.ts.hamilton@gmail.com>
2021-01-14 10:12:22 -0600

    Continued revisions on styling. Added black border to game box. Made a few revisions to game logic.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const main = document.querySelector('#main');
+const chooseX = document.querySelector('.chooseX');
+const chooseO = document.querySelector('.chooseO');
+const choiceDiv = document.querySelector('.makeChoice');
+
+let chosenChar;
+
+chooseX.addEventListener('click', makeChoiceX);
+chooseO.addEventListener('click', makeChoiceO);
 
 let gameBoard = {
-    board: [['x'],['o'],['x'],
-            ['o'],['o'],['x'],
-            ['x'],['x'],['o']]
+    board: [[],[],[],
+            [],[],[],
+            [],[],[]]
 };
 
-function generate(){
- 
+// let gameBoard = {
+//     board: [['x'],['o'],['x'],
+//             ['o'],['o'],['x'],
+//             ['x'],['x'],['o']]
+// };
 
+function makeChoiceX(){
+    chosenChar = 'x';
+    choiceDiv.style.display = 'none';
 }
 
+function makeChoiceO(){
+    chosenChar = 'o';
+    choiceDiv.style.display = 'none';
+}
 
 let displayController = {
 
@@ -18,9 +36,10 @@ let displayController = {
 
 function displayBoard(){
     gameBoard.board.forEach((char)=>{
-        let el = document.createElement('p');
-        el.innerText = char;
-        main.appendChild(el);
+        let el = `
+            <p data-index=1>${char}</p>
+        `;
+        main.innerHTML += el;
     });
 }
 
